9d9d87947075dd0eeb7db2f9c9415fc8c58590da
Gi Leite <giovanna.mleite@hotmail.com>
2022-10-25 22:26:46 -0300

    js-cloneflix

---
+++ app.js
@@ -0,0 +1,17 @@
+let filme01 = 'imagens/filme01.jpg'
+let filme02 = 'imagens/filme02.jpg'
+let filme03 = 'imagens/filme03.jpg'
+let filme04 = 'imagens/donnie-darko.jpg.webp'
+let filme05 = 'imagens/garota-exemplar.jpg.png'
+let filme06 = 'imagens/sem-limites.jpg.jpg'
+document.querySelector(".container").innerHTML += "<img src='" + filme01 +"'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme02 +"'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme03 +"'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme04 +"'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme05 +"'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme06 +"'> "
+
+//1- acrescentar mais 3 filmes
+//2- personalizar o layout (trocar filmes,fundo,cores,fonts)
+//3- armazenar os filmes em um unico vetor (array)
+
